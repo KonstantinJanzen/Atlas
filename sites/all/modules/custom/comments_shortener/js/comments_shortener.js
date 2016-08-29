b3ab8ed7a309cf70a8d3f853c114f0190ef820f6
Friederike Lauterbach <lauter@uni-hildesheim.de>
2016-08-30 00:24:27 +0200

    Add custom module 'comments_shortener'. Trim comments and display them via ellipses.

---
+++ sites/all/modules/custom/comments_shortener/js/comments_shortener.js
@@ -0,0 +1,44 @@
+/**
+ * Created by friederikelauterbach on 29/08/16.
+ */
+
+//Create a Namespace for Indeko javascript objects (no objects in global namespace)
+var Indeko = (function (Indeko) {
+
+    Indeko.Comments = Indeko.Comments || {};
+
+    Indeko.Comments.init = function () {
+
+        $('.comment .comment-content .field-name-comment-body .field-item').each(function (index, comment) {
+            // Trim if the text is longer than 50 characters
+            comment = $(comment);
+            if (comment.text().length > 50) {
+
+                var text = comment.text().trim();
+                comment.text(text.slice(0, 50));
+
+                var a = document.createElement('a');
+                var linkText = document.createTextNode("...");
+                // Ellipses
+                a.appendChild(linkText);
+                a.className = "comment-shortener-link";
+                a.title = "view more";
+                a.addEventListener('click', function (event) {
+                    comment.text(text);
+                });
+                comment.append(a);
+            }
+        });
+    };
+
+    return Indeko;
+
+
+})(Indeko || {});
+
+
+$(document).ready(function () {
+
+    Indeko.Comments.init();
+
+});
