9a4bcaf31e5a27c4a18d4c98951b0bb22cfcedb6
Stephan Mende <stephan.mende@t-online.de>
2016-10-06 13:16:07 +0200

    ID 39: Edit create link interaction and design

--- sites/all/modules/custom/node_creation_links/js/nodecreation.js
+++ sites/all/modules/custom/node_creation_links/js/nodecreation.js
@@ -1,6 +1,29 @@
-// toggle node creation links on click
-jQuery(document).ready(function() {
-    jQuery("#newelement").click(function () {
-        jQuery(".nodeAddLink").toggle();
-    });
-});
+// Global namespace
+var Indeko = Indeko || {};
+$ = jQuery;
+
+
+Indeko.Nodecreation = Indeko.Nodecreation || {
+    /**
+     * Initializes nodecreation.
+     */
+    init: function () {
+      $(document).ready(function () {
+
+        // toggle node creation links on click
+        var createElementLink = $("#newelement");
+        createElementLink.click(function () {
+          $(".nodeAddLink").toggle();
+
+          // toggle css styling on click
+          if ($(this).hasClass('selected')) {
+            $(this).removeClass('selected');
+          } else {
+            $(this).addClass('selected');
+          }
+        });
+      });
+    }
+  };
+
+Indeko.Nodecreation.init();
